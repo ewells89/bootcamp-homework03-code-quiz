983b3fb8bd64f72a4622ea48e298b4a5864d7534
Elyse Wells <cewells89@gmail.com>
2020-10-03 01:09:56 -0400

    added logic to display question and answer content in JS code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Global variable declarations 
 
 var timer = 60;
-var startQuiz = document.getElementById("start");
+var start = document.getElementById("start");
+var quizBox = document.getElementById("quizBox");
 
+var currentQuestion = 0;
 
 // Question and Answers object
 
@@ -29,25 +31,57 @@ var quizQuestions = [
     },
     {
         question:  "A very useful tool used during development and debugging for printing content to the debugger is",
-        answers: ["JavaScript", "terminal/ bash", "for loops", "console log"],
+        answers: ["JavaScript", "terminal/bash", "for loops", "console log"],
         correct: "for loops"
     },
 ]
 
 
 // Start Quiz Function
-startQuiz.addEventListener("click", function (){
+start.addEventListener("click", function (){
     console.log("The quiz has begun!");
-    getQuestion();
+    getContent();
     })
 
 
 
 // Function to retrieve questions
-function getQuestion(){
+function getContent(){
     console.log("First question!")
+    
+        // Clear the content from the previous element
+        quizBox.style.display = "none";
 
-}
+        // Create Div wrapper for question and answers
+        var questionDiv = document.createElement("div")
+        questionDiv.setAttribute("id","quizBox")
+        document.body.append(questionDiv);
+
+            // questionDiv.style.display = "none";
+
+        // Generate the text to display for the question
+        var next = document.createElement("h2");
+        // next.setAttribute("id", "quizBox");
+        next.textContent = quizQuestions[currentQuestion].question;
+        questionDiv.appendChild(next);
+        
+        // Generate the buttons with the possible answers for the question
+        for(var i=0; i < quizQuestions[currentQuestion].answers.length; i++){
+            var answerParagraph = document.createElement("p");
+            questionDiv.appendChild(answerParagraph);
+
+            var answerButton = document.createElement("button");
+            answerButton.setAttribute("type", "button");
+            answerButton.setAttribute("class", "btn");
+            answerButton.setAttribute("id", "choices");
+            answerButton.textContent = quizQuestions[currentQuestion].answers[i];
+            answerParagraph.append(answerButton);
+        }
+
+
+        
+    }
+
 
 // Function to display answer
 function getAnswer(){
