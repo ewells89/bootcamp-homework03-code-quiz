82defb6bdf8f13cd50cad7d5e9c431284c9c55df
Elyse Wells <cewells89@gmail.com>
2020-10-03 21:39:00 -0400

    add logic to cycle through questions when an answer is clicked

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bootcamp-homework03-code-quiz
 Homework Assignment 3: Code Quiz
+
+
+tutorials
+* setAttribute: https://www.w3schools.com/jsref/met_element_setattribute.asp
+* appendChild: https://www.w3schools.com/jsref/met_node_appendchild.asp
+* DOM styling: https://www.w3schools.com/jsref/prop_style_display.asp
--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var timer = 60;
 var start = document.getElementById("start");
 var quizBox = document.getElementById("quizBox");
 
+
+
 var currentQuestion = 0;
 
 // Question and Answers object
@@ -27,67 +29,114 @@ var quizQuestions = [
     {
         question: "String values must be enclosed within ______ when being assigned to variables.",
         answers: ["commas", "curly brackets", "quotes", "parenthesis"],
-        correct: "curly brackets"
+        correct: "quotes"
     },
     {
         question:  "A very useful tool used during development and debugging for printing content to the debugger is",
         answers: ["JavaScript", "terminal/bash", "for loops", "console log"],
-        correct: "for loops"
+        correct: "console log"
     },
 ]
 
 
-// Start Quiz Function
-start.addEventListener("click", function (){
-    console.log("The quiz has begun!");
-    getContent();
-    })
+// EVENT LISTENERS
 
+
+// Listen for Quiz Start
+start.addEventListener("click", 
+    function (){
+        console.log("The quiz has begun!");
+        getContent();
+    }
+)
+
+
+// FUNCTIONS
 
 
 // Function to retrieve questions
 function getContent(){
-    console.log("First question!")
+    // console.log("First question!")
     
-        // Clear the content from the previous element
-        quizBox.style.display = "none";
+    // Clear the content from the previous element
+    quizBox.style.display = "none";
 
-        // Create Div wrapper for question and answers
-        var questionDiv = document.createElement("div")
-        questionDiv.setAttribute("id","quizBox")
-        document.body.append(questionDiv);
+    // Create Div wrapper for question and answers
+    var questionDiv = document.createElement("div")
+    questionDiv.setAttribute("id","quizBox")
+    document.body.append(questionDiv);
 
-            // questionDiv.style.display = "none";
+    // Generate the text to display for the question
+    var next = document.createElement("h2");
+    next.textContent = quizQuestions[currentQuestion].question;
+    questionDiv.appendChild(next);
+    
+    // Generate the buttons with the possible answers for the question
+    for(var i=0; i < quizQuestions[currentQuestion].answers.length; i++){
+        var answerParagraph = document.createElement("p");
+        questionDiv.appendChild(answerParagraph);
 
-        // Generate the text to display for the question
-        var next = document.createElement("h2");
-        // next.setAttribute("id", "quizBox");
-        next.textContent = quizQuestions[currentQuestion].question;
-        questionDiv.appendChild(next);
-        
-        // Generate the buttons with the possible answers for the question
-        for(var i=0; i < quizQuestions[currentQuestion].answers.length; i++){
-            var answerParagraph = document.createElement("p");
-            questionDiv.appendChild(answerParagraph);
-
-            var answerButton = document.createElement("button");
-            answerButton.setAttribute("type", "button");
-            answerButton.setAttribute("class", "btn");
-            answerButton.setAttribute("id", "choices");
-            answerButton.textContent = quizQuestions[currentQuestion].answers[i];
-            answerParagraph.append(answerButton);
-        }
-
-
-        
+        var answerButton = document.createElement("button");
+        answerButton.setAttribute("type", "button");
+        answerButton.setAttribute("class", "btn");
+        answerButton.setAttribute("id", "choices");
+        answerButton.textContent = quizQuestions[currentQuestion].answers[i];
+        answerParagraph.append(answerButton);
     }
 
+    // Event Listener for Choice Buttons
+    questionDiv.addEventListener("click",
+        function(event){
+            if(event.target.matches("button")){
+                console.log("You picked the next question!");
+                // console.log(currentQuestion);
+                
+                getAnswer();
+                // Function to get and display answer
+                function getAnswer(){
+                    questionDiv.style.display = "none";
 
-// Function to display answer
-function getAnswer(){
-    console.log ("The response will be displayed here!");
+                    console.log ("The response will be displayed here!");
+                    var userAnswer = event.target.textContent;
+                    var correctAnswer = quizQuestions[currentQuestion].correct;
+                        console.log(correctAnswer);
+
+
+                    if(userAnswer === correctAnswer){
+                        var messageCorrect = document.createElement("p");
+                        var hr = document.createElement("hr");
+                        
+                        messageCorrect.setAttribute("id","answer");
+                        messageCorrect.textContent= "Correct!";
+                        questionDiv.append(hr);
+                        questionDiv.append(messageCorrect);
+
+                    } else{
+                        var messageIncorrect = document.createElement("p");
+                        var hr = document.createElement("hr");
+
+                        messageIncorrect.setAttribute("id","answer");
+                        messageIncorrect.textContent= "Incorrect! The correct answer was " + correctAnswer +".";
+                        questionDiv.append(hr);
+                        questionDiv.append(messageIncorrect);
+
+                    }
+                    
+                    currentQuestion++;
+                    getContent();
+                }
+            }
+        }
+
+    )
+
+    if(currentQuestion > quizQuestions.length){
+        console.log("this is the end!")
+    }
 
 }
+
+
 
 
 // setInterval(function countdown(){
